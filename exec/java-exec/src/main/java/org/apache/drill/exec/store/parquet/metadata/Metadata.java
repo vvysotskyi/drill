d45acac34a55e99fc19eaf7d8dddbe7b40066aa3
Volodymyr Vysotskyi <vvovyk@gmail.com>
2019-02-28 20:57:07 +0200

    DRILL-6852: Change after code review

--- exec/java-exec/src/main/java/org/apache/drill/exec/store/parquet/metadata/Metadata.java
+++ exec/java-exec/src/main/java/org/apache/drill/exec/store/parquet/metadata/Metadata.java
@@ -536,23 +536,23 @@ public class Metadata {
   /**
    * Serialize parquet metadata to json and write to a file.
    *
-   * @param parquetTableMetadata parquet table or directory metadata
+   * @param parquetMetadata parquet table or directory metadata
    * @param p file path
    * @param fs Drill file system
    * @throws IOException if metadata can't be serialized
    */
-  private void writeFile(Object parquetTableMetadata, Path p, FileSystem fs) throws IOException {
+  private void writeFile(Object parquetMetadata, Path p, FileSystem fs) throws IOException {
     JsonFactory jsonFactory = new JsonFactory();
     jsonFactory.configure(Feature.AUTO_CLOSE_TARGET, false);
     jsonFactory.configure(JsonParser.Feature.AUTO_CLOSE_SOURCE, false);
     ObjectMapper mapper = new ObjectMapper(jsonFactory);
     SimpleModule module = new SimpleModule();
-    if (parquetTableMetadata instanceof ParquetTableMetadata_v3) {
+    if (parquetMetadata instanceof ParquetTableMetadata_v3) {
       module.addSerializer(ColumnMetadata_v3.class, new ColumnMetadata_v3.Serializer());
     }
     mapper.registerModule(module);
     OutputStream os = fs.create(p);
-    mapper.writerWithDefaultPrettyPrinter().writeValue(os, parquetTableMetadata);
+    mapper.writerWithDefaultPrettyPrinter().writeValue(os, parquetMetadata);
     os.flush();
     os.close();
   }
