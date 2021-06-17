165e45d78b83318db9bdbbb3a300df654d0d5a83
Volodymyr Vysotskyi <vvovyk@gmail.com>
2021-06-17 18:35:28 +0300

    DRILL-5177: Fix GroupScanProperty value for mongo

--- contrib/storage-mongo/src/main/java/org/apache/drill/exec/store/mongo/MongoGroupScan.java
+++ contrib/storage-mongo/src/main/java/org/apache/drill/exec/store/mongo/MongoGroupScan.java
@@ -508,7 +508,7 @@ public class MongoGroupScan extends AbstractGroupScan implements
         String json = collection.find().first().toJson(codec);
         approxDiskCost = json.getBytes().length * recordCount;
       }
-      return new ScanStats(GroupScanProperty.EXACT_ROW_COUNT, recordCount, 1, approxDiskCost);
+      return new ScanStats(GroupScanProperty.NO_EXACT_ROW_COUNT, recordCount, 1, approxDiskCost);
     } catch (Exception e) {
       throw new DrillRuntimeException(e.getMessage(), e);
     }
